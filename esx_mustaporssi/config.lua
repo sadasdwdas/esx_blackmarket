0b2e37d3a82b8c20b48e5c970d2b941c0ab39d08
Maistelija <141753153+sadasdwdas@users.noreply.github.com>
2024-11-10 20:24:21 +0200

    config.lua

--- esx_mustaporssi/config.lua
+++ esx_mustaporssi/config.lua
@@ -5,6 +5,7 @@ Config.KaytaAnimaatioita = true
 Config.Kaupat = {
     PistooliKauppa = {
         label = "Pistooli tukku", 
+        labeltarget = "Puhu Simolle", 
         icon = "fas fa-gun",
         NpcSijainti = vec4(159.0797, -985.5057, 30.0919, 229.7686),
         PedModel = 'mp_m_shopkeep_01',
@@ -16,6 +17,7 @@ Config.Kaupat = {
     },
     RynkkyTukku = {
         label = "Rynkky tukku", 
+        labeltarget = "Puhu Reimolle", 
         icon = "fa-solid fa-person-rifle",
         NpcSijainti = vec4(157.7660, -986.8801, 30.0919, 245.5465),
         PedModel = 'mp_m_shopkeep_01',
@@ -27,6 +29,7 @@ Config.Kaupat = {
     },
 erakeskusKauppa = {
     label = "Eräkeskus", 
+    labeltarget = "Puhu Pasille", 
     icon = "fa-sharp fa-regular fa-paw-simple",
     NpcSijainti = vec4(93.3062, -1591.2389, 31.1436, 229.6833),
     PedModel = 'cs_joeminuteman',
